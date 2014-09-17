06d0b56e27045df545940173a31f64365eccc6dc
yliu483 <yucheng.liu@gatech.edu>
2014-09-17 12:53:10 -0400

    回文HelloWorld C++

---
+++ fun/回文C++.cpp
@@ -0,0 +1,18 @@
+///*
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello, world!"<<endl;
+    return 0;
+}
+//*/
+/*//
+}
+;0 nruter    
+;ldne<<"!dlrow ,olleH"<<touc    
+{
+)(niam tni
+;dts ecapseman gnisu
+>maertsoi< edulcni#
+*///
